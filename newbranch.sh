e23ff7c1cef28a9a29da29095d81227fde465b15
tomer_gab <tomeriko01012001@gmail.com>
2022-11-03 18:51:51 +0000

    newbranch

--- newbranch.sh
+++ newbranch.sh
@@ -3,8 +3,9 @@ if [ $? -eq 0 ]
 then 
   git checkout -b $1
   echo "not for realese" > v.txt
-  echo $1 >> v.txt
+  echo "TAG:$1" >> v.txt
   git commit -am"$1"
+  git push -uf origin $1
   git checkout main
 else 
   git checkout $1
