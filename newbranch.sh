75c25cf897d0db7e28d02f2e0cc13ac77805309c
tomer_gab <tomeriko01012001@gmail.com>
2022-11-04 10:55:35 +0000

    gg

--- newbranch.sh
+++ newbranch.sh
@@ -6,8 +6,8 @@ then
   echo "not for realese" > v.txt
   echo "TAG:$1" >> v.txt
   git commit -am"$1"
-  git push -uf origin $1
-  git checkout main
+ # git push -uf origin $1
+#  git checkout main
 else 
   git checkout $1
 fi
