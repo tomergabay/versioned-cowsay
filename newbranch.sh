8ea9be9884a6250f58167622acd1ba1f49079ba1
tomer_gab <tomeriko01012001@gmail.com>
2022-11-04 11:00:44 +0000

    gg

--- newbranch.sh
+++ newbranch.sh
@@ -3,6 +3,8 @@
 if [ $? -eq 0 ] 
 then 
   git checkout -b $1
+  git config --global user.email "tomeriko01012001@gmail.com"
+  git config --global user.name "tomer_gab"
   echo "not for realese" > v.txt
   echo "TAG:$1" >> v.txt
   git commit -am"$1"
